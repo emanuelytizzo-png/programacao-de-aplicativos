d5d8f332f96d7099b53e0ece401b3db2189ab556
Emanuely <emanuelytizzo@gmail.com>
2026-02-18 12:44:25 -0300

    Primeira aula de programação

---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 290 # variável int
+numero_quebrado = 2.35 # variável float
+alice_ta_comendo = True # variável booleana
+texto = 'A aula acabou' # variável de texto 
+
+
+print(texto)
+print(numero)
+print(numero_quebrado)
+print(alice_ta_comendo)
