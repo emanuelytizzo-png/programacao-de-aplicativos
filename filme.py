41fd189f1856aee946e2a8eb4996cb15e97ba386
emanuely <emanuelytizzo@gmail.com>
2026-03-13 08:49:51 -0300

    13/03

---
+++ filme.py
@@ -0,0 +1,4 @@
+filme = ["Esquadrão suicida", "The old guard", "Decendentes", "Donzela","Instrumentos mortais"]
+
+print(f"quarto filme{filme[4]}")
+print(f"primeiro filme{filme[1]}")
---
+++ produtos.py
@@ -0,0 +1,4 @@
+estoque = ["teclado", "mouse", "monitor"]
+print(f"Estoque atual: {estoque}") 
+estoque[2] = "webcam"
+print(f"Estoque atualizado: {estoque}") 
