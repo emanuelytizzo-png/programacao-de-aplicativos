b129e7e9a99a1f08cd6b0733dc15dc42206934f4
emanuely <emanuelytizzo@gmail.com>
2026-05-06 11:56:15 -0300

    o6/05

--- analisador_desempenho.py
+++ analisador_desempenho.py
@@ -1,16 +1,15 @@
-def analisar_vendas(nome, lista_vendas, meta_mensal):
-    total_vendas = sum(lista_vendas)
-    quantidade_vendas = len(lista_vendas)
-    media = total_vendas / quantidade_vendas
-    if media >= meta_mensal:
-        status = "bateu"
-    else:
-        status = "não bateu"
-    return f"O vendedor {nome} teve média de {media:.2f} e {status} a meta."
+def analisar_vendas (nome, lista_vendas, meta_mensal)
+media = (lista_vendas) len (lista_vendas)
 
-nome_vendedor = "Carlos"
+if media => meta_mensal:
+    status = "bateu" 
+else:
+    status =  "não bateu"
+
+return f"O vendedor {nome} teve média de {media:.2f} e {status} a meta."
+
+vendedor = "carlos"
 vendas_carlos = [1200, 1500, 1100, 1900]
 meta = 1400
-
-resultado = analisar_vendas(nome_vendedor, vendas_carlos, meta)
-print(resultado)
+resultado = analisar_vendas(vendedor, vendas_carlos, meta)
+print(resultado)  
--- calculadora_de_imc.py
+++ calculadora_de_imc.py
@@ -1,32 +1,17 @@
-def gerar_relatorio_saude(nome, peso, altura, idade):
-    """
-    Calcula o IMC, determina a classificação e retorna um relatório personalizado.
-    """
-    imc = peso / (altura ** 2)
-    
-    if imc < 18.5:
+def gerar_relatorio_saude (nome, peso, altura, idade):
+if imc < 18.5:
         categoria = "Baixo peso"
-    elif 18.5 <= imc < 25:
+elif 18.5 <= imc <= 24.9:
         categoria = "Normal"
-    elif 25 <= imc < 30:
+elif 25 <= imc <= 29.9:
         categoria = "Sobrepeso"
-    else:
+else:
         categoria = "Obesidade"
-    
-    relatorio = (f"Olá, {nome}! Com {idade} anos e baseando-se nos dados informados "
-                 f"(Peso: {peso}kg, Altura: {altura}m), seu IMC é {imc:.2f}. "
-                 f"Classificação: {categoria}.")
-    return relatorio
+relatorio = (f"Olá, {nome}. Com base nos dados informados (idade: {idade} anos), ")
+             f"seu IMC é {imc:.2f}, o que se classifica como: {categoria}.
 
-if __name__ == "__main__":
-    print("--- Calculadora de IMC com Relatório Completo ---")
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso (kg), ex: 70.5: "))
+altura = float(input("Digite sua altura (m), ex: 1.75: "))
 
-    nome = input("Digite seu nome: ")
-    peso = float(input("Digite seu peso (kg), ex: 70.5: "))
-    altura = float(input("Digite sua altura (m), ex: 1.75: "))
-    idade = int(input("Digite sua idade: "))
-    
-    resultado = gerar_relatorio_saude(nome, peso, altura, idade)
-    print("\n--- Relatório de Saúde ---")
-    print(resultado)
-    
