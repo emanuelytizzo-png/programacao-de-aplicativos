fc18310e2d4d9a43b484f64fa841ced85be487d1
emanuely <emanuelytizzo@gmail.com>
2026-08-19 11:29:04 -0300

    2 trimestre

---
+++ teste-codígo/exercicio03.py
@@ -0,0 +1,12 @@
+def calcular_desconto(preco, percentual):
+    return preco - (preco * percentual / 100)
+
+assert calcular_desconto(100, 0) == 100
+
+assert calcular_desconto(100, 10) == 90
+
+assert calcular_desconto(100, 50) == 50
+
+assert calcular_desconto(100, 100) == 0
+
+assert calcular_desconto(49.90, 10) == 44.91
